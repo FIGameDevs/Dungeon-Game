5a3a973d1a250a40606337575993ad3e970c5d90
REDRIPEAPPLE <33422259+REDRIPEAPPLE@users.noreply.github.com>
2017-11-10 13:46:16 +0100

    Added very basic movement.
    Added very basic movement.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float Speed = 1;
+
+
+	void Update()
+	{
+		print (Input.GetAxis ("Horizontal"));
+		transform.Translate (Time.deltaTime * Input.GetAxis ("Horizontal")*Speed, 0, Time.deltaTime*Input.GetAxis("Vertical")*Speed);
+	}
+}
